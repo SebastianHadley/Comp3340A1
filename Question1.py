70362e68e9b7430023bbe2f90edc8f0b6c85071d
sebastian hadley <sebastian.hadley@hotmail.com>
2022-08-16 17:10:42 +1000

    Working hamming matrix generation checked a random cell.

--- Question1.py
+++ Question1.py
@@ -1,28 +1,50 @@
+import sys
 from typing import Type
 
+import numpy
 import numpy as np
 
 
 def main():
-    data = []
     data = getpresidency()
-    matrixsize = int(len(data) + 1)
-    matrixsize = int(matrixsize)
-    print(matrixsize)
-    base_matrix = np.zeros((matrixsize, matrixsize),)
-    print(base_matrix)
-    print(len(data))
+    jaccard_matrix  = get_base_matrix(data)
+    hamming_matrix = jaccard_matrix
+    print(len(hamming_matrix))
+    print(len(jaccard_matrix))
+    data = get_attributes(data)
+    i = 1
+    a = 1
+    print(jaccard_matrix)
+    while a < len(jaccard_matrix):
+        while i < len(jaccard_matrix):
+            # jaccard_matrix[a][i] = jaccard_binary(data[a-1], data[i-1])
+            hamming_matrix[a][i] = hamming_distance(data[a-1], data[i-1])
+            i += 1
+        a += 1
+        i = 0
+    print(hamming_matrix)
+
+
+def get_base_matrix(data):
+    matrixsize = len(data)+1
+    base_matrix = np.zeros((matrixsize, matrixsize))
     i = 1
     while i < matrixsize:
-        year = data[i-1]
+        year = data[i - 1]
         year = year[0]
         base_matrix[i][0] = int(year)
         base_matrix[0][i] = int(year)
-        i += 1
-    print(base_matrix)
-
-
-    return
+        i+= 1
+    return base_matrix
+def get_attributes(data):
+    i = 0
+    size = len(data[0])-2
+    attributes = [size]
+    while i < len(data):
+        data[i].pop(0)
+        data[i].pop()
+        i+=1
+    return data
 def getpresidency():
     import csv
     file = open('uspresidency.csv')
@@ -35,7 +57,6 @@ def getpresidency():
     rows
     file.close
     return rows
-
 def jaccard_binary(x,y):
     # A function for finding the similarity between two binary values
     intersection = np.logical_and(x, y)
